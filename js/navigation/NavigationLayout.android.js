70f679ec1835067947a5bef9b05e865e3988ac39
Scott <scottdlivingstone@gmail.com>
2018-03-01 13:12:11 -0800

    Add icons to drawer nav

--- js/navigation/NavigationLayout.android.js
+++ js/navigation/NavigationLayout.android.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { colors } from "../config/style";
 import Router from "./routes";
 import {
   StackNavigation,
@@ -28,6 +30,7 @@ export default class DrawerNavigationLayout extends Component {
         <DrawerNavigationItem
           id="faves"
           selectedStyle={styles.selectedItemStyle}
+          renderIcon={isSelected => this.renderIcon(isSelected, "md-heart")}
           renderTitle={isSelected => this._renderTitle("Faves", isSelected)}
         >
           <StackNavigation id="home" initialRoute={Router.getRoute("faves")} />
@@ -36,6 +39,9 @@ export default class DrawerNavigationLayout extends Component {
         <DrawerNavigationItem
           id="about"
           selectedStyle={styles.selectedItemStyle}
+          renderIcon={isSelected =>
+            this.renderIcon(isSelected, "md-information-circle")
+          }
           renderTitle={isSelected => this._renderTitle("About", isSelected)}
         >
           <StackNavigation id="about" initialRoute={Router.getRoute("about")} />
@@ -43,6 +49,7 @@ export default class DrawerNavigationLayout extends Component {
         <DrawerNavigationItem
           id="schedule"
           selectedStyle={styles.selectedItemStyle}
+          renderIcon={isSelected => this.renderIcon(isSelected, "md-calendar")}
           renderTitle={isSelected => this._renderTitle("Schedule", isSelected)}
         >
           <StackNavigation
@@ -57,6 +64,11 @@ export default class DrawerNavigationLayout extends Component {
 
   _renderHeader = () => {
     return <View style={styles.header} />;
+  };
+
+  renderIcon = (isSelected, icon) => {
+    const iconColor = isSelected ? colors.white : colors.mediumGrey;
+    return <Icon style={{ color: iconColor }} size={24} name={icon} />;
   };
 
   _renderTitle(text, isSelected) {
