7b65a5a64cb5d3f2253263ccbdf2e08107fdc28e
Scott <scottdlivingstone@gmail.com>
2018-03-03 15:46:07 -0800

    Redo map component and add style sheet to acutally make the map show up

--- js/scenes/Map/map.js
+++ js/scenes/Map/map.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { View, Dimensions, ScrollView, StyleSheet } from "react-native";
+
 import MapView from "react-native-maps";
-import { View, Dimensions } from "react-native";
-let { width, height } = Dimensions.get("window");
+
+const { width, height } = Dimensions.get("window");
+const SCREEN_WIDTH = width;
+const SCREEN_HEIGHT = height;
 const ASPECT_RATIO = width / height;
-const LATITUDE = 0;
-const LONGITUDE = 0;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class Map extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
       region: {
         latitude: LATITUDE,
@@ -21,18 +26,45 @@ export default class Map extends Component {
     };
   }
 
-  onRegionChange(region) {
-    this.setState({ region });
-  }
   render() {
     return (
-      <View>
-        <MapView
-          region={this.state.region}
-          provider="google"
-          onRegionChange={this.onRegionChange}
-        />
+      <View style={styles.container}>
+        <ScrollView
+          style={StyleSheet.absoluteFill}
+          contentContainerStyle={styles.scrollview}
+        >
+          <MapView
+            provider={this.props.provider}
+            style={styles.map}
+            scrollEnabled={true}
+            zoomEnabled={true}
+            pitchEnabled={true}
+            rotateEnabled={true}
+            initialRegion={this.state.region}
+          >
+            <MapView.Marker
+              title="This is a title"
+              description="This is a description"
+              coordinate={this.state.region}
+            />
+          </MapView>
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  scrollview: {
+    alignItems: "center"
+  },
+  map: {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT
+  }
+});
